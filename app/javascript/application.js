2205199ffd963c07fd74b831e271e5ff44ab734b
Doyle Baxter <doylembbaxter@gmail.com>
2023-10-01 14:48:28 -0400

    Still trying

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
 import { application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const context = require.context(".", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
