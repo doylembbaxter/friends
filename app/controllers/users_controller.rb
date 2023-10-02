3526aa8cb5efc104c26446ef197d2db5e771edd3
Doyle Baxter <doylembbaxter@gmail.com>
2023-10-02 15:46:12 -0400

    Profile Pic Things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   
     def show
       @user = User.find(params[:id])
-      redirect_to root_path, alert: "Access denied!" unless @user == current_user
+      redirect_to root_path, notice: "Access denied!" unless @user == current_user
     end
   end  
